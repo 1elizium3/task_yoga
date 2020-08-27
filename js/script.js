a7ac62873bf25b5d093c9962b120bba3c860a162
1elizium3 <53572780+1elizium3@users.noreply.github.com>
2020-08-27 13:40:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -331,40 +331,6 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    //  // Подключить скрипт отправки данных с формы к Контактной форме
-    //     contacts.addEventListener('submit', function(event) {
-    //         event.preventDefault();
-    //         contacts.appendChild(statusMessage);
-
-    //         let request = new XMLHttpRequest();
-    //         request.open('POST', 'server.php');
-    //         request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-            
-
-    //         let form2 = new FormData(contacts);
-    //         let obj = {};
-    //         form2.forEach((value, key) => {
-    //             obj[key] = value;
-    //         });
-    //         let json = JSON.stringify(obj);
-
-    //         request.send(json);
-
-    //         request.addEventListener('readystatechange', function() {
-    //             if (request.readyState < 4) {
-    //                 statusMessage.innerHTML = message.loading;
-    //             } else if (request.readyState == 4 && request.status == 200) {
-    //                 statusMessage.innerHTML = message.success;
-    //             } else {
-    //                 statusMessage.innerHTML = message.failure;
-    //             }
-    //         });
-
-    //         for (let i = 0; i < input.length; i++) {
-    //             input[i].value = '';
-    //         }
-    //     });
-
 });
 
 
